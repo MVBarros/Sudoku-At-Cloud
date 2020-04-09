5ef4e650125ccbf057ac2ec95700ffba6b52978c
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-09 12:27:50 +0100

    Compile scripts

---
+++ compile-rnl.sh
@@ -0,0 +1,7 @@
+source java-config-rnl.sh
+cd sudoku
+javac pt/ulisboa/tecnico/cnv/solver/*.java
+javac pt/ulisboa/tecnico/cnv/server/*.java
+cd ..
+javac BIT/samples/*.java
+
---
+++ compile.sh
@@ -0,0 +1,7 @@
+source java-config.sh
+cd sudoku
+javac pt/ulisboa/tecnico/cnv/solver/*.java
+javac pt/ulisboa/tecnico/cnv/server/*.java
+cd ..
+javac BIT/samples/*.java
+
