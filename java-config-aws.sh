c9eb71d6c3ffcdc32af500e7366480d293ea0d30
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-14 12:17:49 +0100

    AWS Files

---
+++ java-config-aws.sh
@@ -0,0 +1,10 @@
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk/
+export JAVA_ROOT=/usr/lib/jvm/java-7-openjdk/
+export JDK_HOME=/usr/lib/jvm/java-7-openjdk/
+export JRE_HOME=/usr/lib/jvm/java-7-openjdk/jre
+export PATH=/usr/lib/jvm/java-7-openjdk/bin/:$PATH
+export SDK_HOME=/usr/lib/jvm/java-7-openjdk/
+export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
+
+export CLASSPATH="$CLASSPATH:/home/ec2-user/Sudoku-At-Cloud/BIT/BIT:/home/ec2-user/Sudoku-At-Cloud/BIT/samples:/home/ec2-user/Sudoku-At-Cloud/BIT:/home/ec2-user/Sudoku-At-Cloud/sudoku"
+
---
+++ upload.sh
@@ -0,0 +1,1 @@
+scp -r -i "aws/CNV-Project-Pair.pem" .  ec2-user@ec2-3-84-127-219.compute-1.amazonaws.com:~ec2-user
