0d3ad2b5f28c1f95ebe72d0887247006301a58da
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-17 16:03:01 +0100

    hfasoifhaso:

--- upload.sh
+++ upload.sh
@@ -1,1 +1,5 @@
+rm graphics out/* README* BIT/samples/*.class BIT/examples/out/*.class BIT/examples/*.class -r
+
 scp -r -i "aws/CNV-Project-Pair.pem" .  ec2-user@ec2-3-84-127-219.compute-1.amazonaws.com:~ec2-user
+
+git stash
