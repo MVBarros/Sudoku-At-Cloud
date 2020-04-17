3fd74fb4e4cf201b904d24b30fe52cb60257ae98
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-17 15:47:12 +0100

    compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,12 @@
 source java-config.sh
 javac sudoku/pt/ulisboa/tecnico/cnv/solver/*.java
 javac BIT/samples/metrics/tools/*.java
-java metrics.tools.SudokuMetricsTool sudoku/pt/ulisboa/tecnico/cnv/solver
+java metrics.tools.SudokuMetricsBFS sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverBFS.class
+java metrics.tools.SudokuMetricsDLX sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverDLX.class
+java metrics.tools.SudokuMetricsCP sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverCP.class
+java metrics.tools.SudokuMetricsDLX 'sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverDLX$AlgorithmXSolver.class'
+java metrics.tools.SudokuMetricsDLX 'sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverDLX$AlgorithmXSolver$Node.class'
+java metrics.tools.SudokuMetricsDLX 'sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverDLX$AlgorithmXSolver$ColumnNode.class'
+java metrics.tools.SudokuMetricsDLX 'sudoku/pt/ulisboa/tecnico/cnv/solver/SudokuSolverDLX$AlgorithmXSolver$ColumnID.class'
 javac sudoku/pt/ulisboa/tecnico/cnv/server/*.java
 java pt.ulisboa.tecnico.cnv.server.WebServer
