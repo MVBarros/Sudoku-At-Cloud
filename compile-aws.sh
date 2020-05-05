8e84bd886d1333c9aa4a25523818a79de785d14b
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-05-05 14:00:30 +0100

    Fix compile aws script

--- compile-aws.sh
+++ compile-aws.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/Sudoku-At-Cloud/java-config.sh
 javac /home/ec2-user/Sudoku-At-Cloud/sudoku/pt/ulisboa/tecnico/cnv/solver/*.java
-javac /home/ec2-user/Sudoku-At-Cloud/BIT/samples/*.java
+javac /home/ec2-user/Sudoku-At-Cloud/BIT/samples/metrics/tools/*.java
 java SudokuMetricsTool /home/ec2-user/Sudoku-At-Cloud/sudoku/pt/ulisboa/tecnico/cnv/solver
 javac /home/ec2-user/Sudoku-At-Cloud/sudoku/pt/ulisboa/tecnico/cnv/server/*.java
 java pt.ulisboa.tecnico.cnv.server.WebServer
