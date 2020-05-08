33622a602ba763a6d506461f95cca1d7ed254f3a
Miguel Barros <miguel@acer-laptop.lan>
2020-05-08 09:53:35 +0100

    AWS JDK
    AWS Credentials
    
    java aws sdk
    
    Java sdk aws
    
    remove aws jdk

--- java-config-aws.sh
+++ java-config-aws.sh
@@ -5,6 +5,6 @@ export JRE_HOME=/usr/lib/jvm/java-7-openjdk/jre
 export PATH=/usr/lib/jvm/java-7-openjdk/bin/:$PATH
 export SDK_HOME=/usr/lib/jvm/java-7-openjdk/
 export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
-
 export CLASSPATH="$CLASSPATH:/home/ec2-user/Sudoku-At-Cloud/BIT/BIT:/home/ec2-user/Sudoku-At-Cloud/BIT/samples:/home/ec2-user/Sudoku-At-Cloud/BIT:/home/ec2-user/Sudoku-At-Cloud/sudoku"
+export CLASSPATH="$CLASSPATH:/home/ec2-user/Sudoku-At-Cloud/aws-java-sdk-1.11.778/lib/aws-java-sdk-1.11.778.jar:/home/ec2-user/Sudoku-At-Cloud/aws-java-sdk-1.11.778/third-party/lib/*"
 
--- java-config.sh
+++ java-config.sh
@@ -7,3 +7,4 @@ export SDK_HOME=/usr/lib/jvm/java-7-openjdk/
 export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
 
 export CLASSPATH="$CLASSPATH:`pwd`/BIT/BIT:`pwd`/BIT/samples:`pwd`/BIT:`pwd`/sudoku"
+export CLASSPATH="$CLASSPATH:`pwd`/aws-java-sdk-1.11.778/lib/aws-java-sdk-1.11.778.jar:`pwd`/aws-java-sdk-1.11.778/third-party/lib/*"
