7c9393972d7d88bef8a19c30866cd49c87f62920
MVBarros98 <miguel.v.barros@tecnico.ulisboa.pt>
2020-05-09 12:27:58 +0100

    Only upload stats if they didn't exist

--- dynamo/src/pt/ulisboa/tecnico/cnv/dynamo/DynamoFrontEnd.java
+++ dynamo/src/pt/ulisboa/tecnico/cnv/dynamo/DynamoFrontEnd.java
@@ -15,20 +15,19 @@ import pt.ulisboa.tecnico.cnv.solver.SolverArgumentParser;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class DynamoFrontEnd {
     private static final String REGION = "us-east-1";
 
-    private static final String KEY_ATTRIBUTE_NAME = "key";
-
-    private static final String KEY_BOARD_SIZE = "Board-Size";
-    private static final String KEY_UNASSIGNED_ENTRIES = "Unassigned-Entries";
-    private static final String KEY_BOARD_IDENTIFIER = "Board-Identifier";
-    private static final String KEY_UN_PARAMETER = "UN-Parameter";
-    private static final String KEY_REQUEST_COST = "Request-Cost";
+    private static final String KEY_ENTRY_UNIQUE_ID = "EntryKey";
+    private static final String KEY_BOARD_SIZE = "BoardSize";
+    private static final String KEY_UNASSIGNED_ENTRIES = "UnassignedEntries";
+    private static final String KEY_BOARD_IDENTIFIER = "BoardIdentifier";
+    private static final String KEY_UN_PARAMETER = "UNParameter";
+    private static final String KEY_REQUEST_COST = "RequestCost";
 
     private static AmazonDynamoDB dynamoDB;
+
     static {
         //Start dynamo DB on class load.
         ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
@@ -59,29 +58,37 @@ public class DynamoFrontEnd {
     }
 
     public static void uploadStats(SolverArgumentParser parser, Stats stats) {
+        String boardZeros = Integer.toString(MetricUtils.getBoardZeros(parser));
+        String key = getKey(parser, boardZeros);
         Map<String, AttributeValue> item = new HashMap<>();
         item.put(KEY_BOARD_SIZE, new AttributeValue().withN(Integer.toString(parser.getN1())));
         item.put(KEY_BOARD_IDENTIFIER, new AttributeValue(parser.getInputBoard()));
         item.put(KEY_UN_PARAMETER, new AttributeValue().withN(Integer.toString(parser.getUn())));
-        item.put(KEY_UNASSIGNED_ENTRIES, new AttributeValue().withN(Integer.toString(MetricUtils.getBoardZeros(parser))));
+        item.put(KEY_UNASSIGNED_ENTRIES, new AttributeValue().withN(boardZeros));
         item.put(KEY_REQUEST_COST, new AttributeValue().withN(Long.toString(stats.getCost())));
-        item.put(KEY_ATTRIBUTE_NAME, new AttributeValue(randomKey()));
-        PutItemRequest putItemRequest = new PutItemRequest(stats.getTableName(), item);
-        dynamoDB.putItem(putItemRequest);
+        item.put(KEY_ENTRY_UNIQUE_ID, new AttributeValue(key));
+        PutItemRequest putItemRequest = new PutItemRequest(stats.getTableName(), item)
+                .withConditionExpression(String.format("attribute_not_exists(%s)", KEY_ENTRY_UNIQUE_ID));
+        try {
+            dynamoDB.putItem(putItemRequest);
+        } catch(ConditionalCheckFailedException e) {
+            //Item with that key already exists
+            System.out.println("Key already existed: " + key);
+        }
     }
 
     private static void createStatsTable(String name) throws InterruptedException {
         CreateTableRequest createTableRequest = new CreateTableRequest().withTableName(name)
-                .withKeySchema(new KeySchemaElement().withAttributeName(KEY_ATTRIBUTE_NAME).withKeyType(KeyType.HASH))
-                .withAttributeDefinitions(new AttributeDefinition().withAttributeName(KEY_ATTRIBUTE_NAME).withAttributeType(ScalarAttributeType.S))
+                .withKeySchema(new KeySchemaElement().withAttributeName(KEY_ENTRY_UNIQUE_ID).withKeyType(KeyType.HASH))
+                .withAttributeDefinitions(new AttributeDefinition().withAttributeName(KEY_ENTRY_UNIQUE_ID).withAttributeType(ScalarAttributeType.S))
                 .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L));
 
         TableUtils.createTableIfNotExists(dynamoDB, createTableRequest);
         TableUtils.waitUntilActive(dynamoDB, name);
     }
 
-    private static String randomKey() {
-        return UUID.randomUUID().toString();
+    private static String getKey(SolverArgumentParser parser, String boardZeros) {
+        return String.format("N1:%s&N2:%s&UN:%s&BOARD:%s&ZEROS:%s", parser.getN1(), parser.getN2(), parser.getUn(), parser.getInputBoard(), boardZeros);
     }
 
 
