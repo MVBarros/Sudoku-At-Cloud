eb286d236040c0c53964c24e407fdb19fe4a91ed
MVBarros98 <miguel.v.barros@tecnico.ulisboa.pt>
2020-05-15 17:05:46 +0100

    Compile

---
+++ compile-lb-aws.sh
@@ -0,0 +1,5 @@
+source java-config-aws.sh
+
+javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*.java
+javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*/*.java
+java pt.ulisboa.tecnico.cnv.loadbalancer.Main
---
+++ compile-lb-rnl.sh
@@ -0,0 +1,5 @@
+source java-config-rnl.sh
+
+javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*.java
+javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*/*.java
+java pt.ulisboa.tecnico.cnv.loadbalancer.Main
