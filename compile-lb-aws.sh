cc7be55085cd984613a7eefc90bf838be2277796
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-05-18 19:36:38 +0100

    Added scripts for loadbalancer compilation

--- compile-lb-aws.sh
+++ compile-lb-aws.sh
@@ -1,5 +1,6 @@
-source java-config-aws.sh
-
-javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*.java
-javac loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*/*.java
+source /home/ec2-user/Sudoku-At-Cloud/java-config-aws.sh
+javac /home/ec2-user/Sudoku-At-Cloud/loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*.java
+javac /home/ec2-user/Sudoku-At-Cloud/loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/*/*.java
+javac /home/ec2-user/Sudoku-At-Cloud/autoscaler/src/pt/ulisboa/tecnico/cnv/autoscaler/*.java
+javac /home/ec2-user/Sudoku-At-Cloud/autoscaler/src/pt/ulisboa/tecnico/cnv/autoscaler/*/*.java
 java pt.ulisboa.tecnico.cnv.loadbalancer.Main
