0e5d25172a22bf25d5ab1209d76c063156f0d617
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-05-18 10:43:59 +0100

    Main initiates minimum number of instances

--- loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/Main.java
+++ loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/Main.java
@@ -1,6 +1,7 @@
 package pt.ulisboa.tecnico.cnv.loadbalancer;
 
 import com.sun.net.httpserver.HttpServer;
+import pt.ulisboa.tecnico.cnv.autoscaler.task.CreateInstanceTask;
 import pt.ulisboa.tecnico.cnv.dynamo.DynamoFrontEnd;
 import pt.ulisboa.tecnico.cnv.loadbalancer.task.ThreadManager;
 
@@ -9,10 +10,14 @@ import java.util.concurrent.Executors;
 
 public class Main {
     private static final int PORT = 9000;
+    private static final int MIN_COUNT = 1;
 
     public static void main(final String[] args) throws Exception {
         DynamoFrontEnd.createTables();
 
+        for(int instance = 0; instance < MIN_COUNT; instance++){
+            ThreadManager.execute(new CreateInstanceTask());
+        }
         final HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
         server.createContext("/newInstance", new NewInstanceHandler());
         server.createContext("/sudoku", new SudokuHandler());
